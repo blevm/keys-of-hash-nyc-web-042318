b22c55f91eec65c737452c176aaee0a129c4049f
Michelle Blevins <mjblevins7@gmail.com>
2018-04-12 03:49:11 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,7 +1,7 @@
 class Hash
   def keys_of(*arguments)
     things_array = []
-    self.each do |key, value|
+    self.collect do |key, value|
       if value == arguments
         things_array.push(key)
       end
