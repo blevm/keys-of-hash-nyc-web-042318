fbf13ee8c7d6a6c7af1d1a246b74f06bb6a87979
Michelle Blevins <mjblevins7@gmail.com>
2018-04-12 03:37:29 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,10 @@
 class Hash
+  animals = []
   def keys_of(arguments)
-    Array.new
+    Hash.each do |key, value|
+      if value == arguments
+        animals << key
+      end
+    end
   end
 end
