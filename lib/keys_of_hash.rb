c4b8076842500df57232eaae4791eee4f69ae27c
Michelle Blevins <mjblevins7@gmail.com>
2018-04-12 03:41:16 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,11 +1,5 @@
 class Hash
-  animal_list = []
-  def keys_of(arguments)
-    Hash.each do |key, value|
-      if value == arguments
-        animals << key
-      end
-    end
-    animal_list
+  def keys_of(**arguments) 
+    return arguments.inspect
   end
 end
